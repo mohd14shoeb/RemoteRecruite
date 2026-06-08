cd295e69ab17467b1ebfe110ef835fbb453e6548
Shoeb Khan <mohd14shoeb@gmail.com>
2026-06-08 12:09:29 +0530

    Initial Commit

---
+++ RemoteRecruit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemoteRecruit
+//
+//  Created by Shoeb Khan on 08/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
